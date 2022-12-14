a9993876a957f09048ef0281b1d49b259ff2cf98
tugasbesardaspro <tugasbesar920@gmail.com>
2022-12-14 20:01:40 +0700

    sixth commit

--- pages/Youtube-downloader.py
+++ pages/Youtube-downloader.py
@@ -2,19 +2,17 @@ import streamlit as st
 from pytube import YouTube
 
 
+link = './components/youtube_downloader/'
 
-link = "https://github.com/tugasbesardaspro/tubesdaspro/tree/main/components/youtube_downloader/"
-
-st.image(f'./components/youtube_downloader/yt.png',use_column_width=True)
+st.image(f'./components/youtube_downloader/yt.png', use_column_width=True)
 st.header("APLIKASI DOWNLOAD VIDEO DAN AUDIO YOUTUBE")
-
 
 
 video_url = st.text_input("Masukkan Link Video Youtube yang anda salin")
 
 genre = st.radio(
-"Pilih Format of Video",
-('360 P', '720 P', '1080 P', 'audio/mp4 (128 kbps)', 'audio/webm (50 kbps)', 'audio/webm (70 kbps)', 'audio/webm (160 kbps)'))
+    "Pilih Format of Video",
+    ('360 P', '720 P', '1080 P', 'audio/mp4 (128 kbps)', 'audio/webm (50 kbps)', 'audio/webm (70 kbps)', 'audio/webm (160 kbps)'))
 
 
 if video_url:
@@ -26,56 +24,56 @@ if video_url:
     {}
     ## Length: {} seconds
     ## Rating: {} 
-    '''.format(video.title , video.length , video.rating))
+    '''.format(video.title, video.length, video.rating))
 
     if genre == '360 P':
         video_streams = video.streams.filter(file_extension='mp4')
-        getId = video_streams.get_by_itag(18)        
+        getId = video_streams.get_by_itag(18)
         subtype = 'mp4'
         btntype = 'video'
-    elif genre == '720 P':
+    if genre == '720 P':
         video_streams = video.streams.filter(file_extension='mp4')
         getId = video_streams.get_by_itag(22)
         subtype = 'mp4'
         btntype = 'video'
-    elif genre == '1080 P':
+    if genre == '1080 P':
         video_streams = video.streams.filter(file_extension='mp4')
         getId = video_streams.get_by_itag(137)
         subtype = 'mp4'
         btntype = 'video'
-    elif genre == 'audio/mp4 (128 kbps)':
+    if genre == 'audio/mp4 (128 kbps)':
         video_streams = video.streams.filter(only_audio=True)
         getId = video_streams.get_by_itag(140)
         subtype = 'mp4'
         btntype = 'audio'
-    elif genre == 'audio/webm (50 kbps)':
+    if genre == 'audio/webm (50 kbps)':
         video_streams = video.streams.filter(only_audio=True)
         getId = video_streams.get_by_itag(249)
         subtype = 'webm'
         btntype = 'audio'
-    elif genre == 'audio/webm (70 kbps)':
+    if genre == 'audio/webm (70 kbps)':
         video_streams = video.streams.filter(only_audio=True)
         getId = video_streams.get_by_itag(250)
         subtype = 'webm'
         btntype = 'audio'
-    elif genre == 'audio/webm (160 kbps)':
+    if genre == 'audio/webm (160 kbps)':
         video_streams = video.streams.filter(only_audio=True)
         getId = video_streams.get_by_itag(251)
         subtype = 'webm'
         btntype = 'audio'
 
     if getId:
-        getId.download(f"{link}download")        
+        getId.download(f"{link}download")
         video_file = open(f"{link}download/{video.title}.{subtype}", "rb")
         video_bytes = video_file.read()
 
         st.video(video_bytes)
-        
+
         video.title
 
     with open(f"{link}download/{video.title}.{subtype}", "rb") as file:
         btn = st.download_button(
-                label=f"Download {btntype}",
-                data=file,
-                file_name=f"{video.title}.{subtype}",            
-            )
+            label=f"Download {btntype}",
+            data=file,
+            file_name=f"{video.title}.{subtype}",
+        )
