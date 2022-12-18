4a0bd3167b3bb00722700bb01dcc9cd0deea560e
tugasbesardaspro <tugasbesar920@gmail.com>
2022-12-18 15:13:21 +0700

    allslas

--- Home.py
+++ Home.py
@@ -8,3 +8,32 @@ st.write("Aplikasi operator all in one sederhana yang memuat berbagai macam perh
 st.subheader("Kelompok : \n - Febriadi Lesmana / 3312201062  \n - Jessica Intan Kinasih / 3312201063 \n - Hugo Hamonangan / 3312201066 \n - Janrianto Rizki Pasaribu / 3312201077 \n - Sherina Nur Syabania / 3312201078 \n - Nurhadi / 3312201082")
 
 st.write("Untuk menggunakan aplikasi ini silahkan klik sidebar pada icon panah di pojok kiri atas.")
+
+page = """
+<style>
+[data-testid="stSidebar"],
+[data-testid="stHeader"],
+.egzxvld3{
+background: #0052D4;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #6FB1FC, #4364F7, #0052D4);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #6FB1FC, #4364F7, #0052D4); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
+.e1tzin5v0{
+/* From https://css.glass */
+background: rgba(55, 52, 52, 0.23);
+border-radius: 16px;
+box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+backdrop-filter: blur(12.9px);
+-webkit-backdrop-filter: blur(12.9px);
+border: 1px solid rgba(55, 52, 52, 0.05);
+padding: 2rem;
+}
+
+.e1tzin5v0 p {
+  width: 90%
+}
+</style>
+"""
+
+st.markdown(page, unsafe_allow_html=True)
